ad200e9db82adaabbabd233f9b74affc9bfe6c39
jcerezo <jcb1@um.es>
2016-08-02 13:30:50 +0200

    Add support to commented files and onecole plots
    It can now read files ignoring the comments if any
    The support t onecol plots is give by the -type [xy|y] option

--- average_plots.f90
+++ average_plots.f90
@@ -8,7 +8,7 @@ program average_plots
     real(8),dimension(100) :: yy,y, x, w
     real(8) :: a, da, da0, factor
     !Counters
-    integer :: i,j,k
+    integer :: i,j,k, ii
     integer :: ispc, iw, ispc_tot
     integer :: N, nadd
     !IO
@@ -16,7 +16,9 @@ program average_plots
     !Input selections stuff
     logical :: argument_retrieved, &
                do_weight=.false.   
-    character(len=200) :: arg
+    character(len=200) :: arg, line
+    character(len=2)   :: spctype
+    character(len=1)   :: cnull
 
     !Initialize
     nadd=0
@@ -37,6 +39,7 @@ program average_plots
                 open(10,file=arg,status='old',iostat=iostatus)
                 if (iostatus /= 0) then
                     write(0,*) "ERROR: opening "//trim(adjustl(arg))
+                    call close_scr_files(ispc)
                     stop
                 endif
                 iw=0
@@ -51,6 +54,9 @@ program average_plots
                 argument_retrieved=.true.
                 call getarg(i+1, arg)
                 read(arg,*) nadd
+            case ("-type")
+                argument_retrieved=.true.
+                call getarg(i+1, spctype)
             case ("-h")
                 print*, "Program to average spectra"
                 print*, "All plots need to be defined in the same time grid."
@@ -61,7 +67,9 @@ program average_plots
                 print*, "          obtained by linear extrapolation. It is"
                 print*, "          assumed that the imput set is evenly spaced"
                 print*, "          Weights should include the intermediante points"
+                print*, "  -type [xy|y] Type of calculation (default: xy)"
                 print*, ""
+                call close_scr_files(ispc)
                 stop
             case default
                 ispc = ispc + 1
@@ -69,8 +77,20 @@ program average_plots
                 open(10+ispc,file=filenames(ispc),status='old',iostat=iostatus)
                 if (iostatus /= 0) then
                     write(0,*) "ERROR: opening spectrum "//trim(adjustl(arg))
+                    call close_scr_files(ispc)
                     stop
                 endif
+                ! Preprocess the file to get rid out of comment lines
+                open(300+ispc,status="scratch")
+                do
+                    read(10+ispc,'(A)',iostat=iostatus) line
+                    if (iostatus/=0) exit
+                    call split_line(line,"#",line,cnull) 
+                    if ( len_trim(line) == 0 ) cycle
+                    write(300+ispc,'(A)') trim(adjustl(line))
+                enddo
+                close(10+ispc)
+                rewind(300+ispc)
         end select
     enddo
 
@@ -107,6 +127,7 @@ program average_plots
         write(0,*) "     Weights     Spectra"
         write(0,*) iw, ispc_tot
         write(0,*) "ERROR: number of weights is not the same as number of spectra + added points"
+        call close_scr_files(ispc)
         stop
     else
         write(0,*) " Spectrum                        Weight    NormWeight"
@@ -124,10 +145,18 @@ program average_plots
     enddo
     write(0,'(X,A,F12.4)') "Sum of normalized weights = ", a
 
+    ii=0
     do
         do i=1,ispc
-            read(10+i,*,iostat=iostatus) x(i), y(i)
-            if (iostatus /= 0) stop
+            if (adjustl(spctype) == "y") then
+                read(300+i,*,iostat=iostatus) y(i)
+            else
+                read(300+i,*,iostat=iostatus) x(i), y(i)
+            endif
+            if (iostatus /= 0) then
+                call close_scr_files(ispc)
+                stop
+            endif
         enddo
         ! Check that all x have the same value
         do i=2,ispc
@@ -135,6 +164,7 @@ program average_plots
             if (abs(x(1)-x(i))/x(1) > 0.01) then
                 write(0,*) "ERROR: the spectra have different scales"
                 write(0,*) x(1), x(i), i
+                call close_scr_files(ispc)
                 stop
             endif
         enddo 
@@ -144,16 +174,16 @@ program average_plots
         do i=1,ispc-1
             k=k+1
             yy(k) = y(i)
-write(100+k,*) x(1), yy(k)
+!write(100+k,*) x(1), yy(k)
             do j=1,nadd
                 k=k+1
                 yy(k) = y(i) + (y(i+1)-y(i))*float(j)/float(nadd+1)
-write(100+k,*) x(1), yy(k)
+!write(100+k,*) x(1), yy(k)
             enddo
         enddo
         k=k+1
         yy(k) = y(ispc)
-write(100+k,*) x(1), yy(k)
+!write(100+k,*) x(1), yy(k)
         
         ! Compute the average
         a = 0.d0
@@ -174,10 +204,65 @@ write(100+k,*) x(1), yy(k)
         !enddo
         !da0 = dsqrt(da0/float(ispc_tot-1))
 
-        print*, x(1), a, da !, da0
+        if (adjustl(spctype) == "y") then
+            print*, ii, a, da !, da0
+            ii = ii + 1
+        else
+            print*, x(1), a, da !, da0
+        endif
     enddo
 
+    call close_scr_files(ispc)
+
     stop
+ 
+    contains
+
+    subroutine split_line(line,splitter,line_a,line_b)
+
+        !Split a line from a given marker. If it is not present, it does not
+        !split the line (the whole is preserved in line_a
+
+        character(len=*),intent(in):: line,splitter
+        character(len=*),intent(out):: line_a,line_b
+
+        !local
+        integer :: i,j
+        !Auxiliar helps when line(input) is also one 
+        !of the outputs, line_a or line_b
+        character(len=(len(line_a))) :: aux_line_a
+
+        i=INDEX(line,splitter)
+        if ( i == 0 ) then
+            line_a=line
+            line_b=""
+            return
+        endif
+        j=len_trim(splitter)
+        
+        aux_line_a=line(1:i-1)
+        line_b=line(i+j:)
+        line_a=aux_line_a
+
+        return
+
+    end subroutine split_line
+ 
+    subroutine close_scr_files(n)
+
+        integer,intent(in) :: n
+
+        !Local
+        integer :: i
+
+        do i=1,n
+            close(300+i)
+        enddo
+
+        return
+
+    end subroutine close_scr_files
+
 
 end program average_plots
 
